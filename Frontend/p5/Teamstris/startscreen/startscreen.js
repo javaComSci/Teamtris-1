16e6b87716f3d46afa6138a4b53971af88b9a4c8
Dellamoresteven <dellamoresteven@gmail.com>
2020-01-21 20:41:07 -0500

    Startscreen started

---
+++ Frontend/p5/Teamstris/startscreen/startscreen.js
@@ -0,0 +1,9 @@
+class StartScreen {
+  constructor() {
+    if(startscreen_constructor) console.log("Creating StartScreen Object");
+  }
+
+  draw() {
+    if(startscreen_draw) console.log("Drawing on Startscreen");
+  }
+}
