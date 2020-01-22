fbc3c6a1dbb2233a841bdab65661ae0d3785ceaa
Dellamoresteven <dellamoresteven@gmail.com>
2020-01-21 23:13:24 -0500

    Added a button framework to create buttons easily, hopefully

--- Frontend/p5/Teamstris/startscreen/startscreen.js
+++ Frontend/p5/Teamstris/startscreen/startscreen.js
@@ -1,9 +1,35 @@
 class StartScreen {
   constructor() {
     if(startscreen_constructor) console.log("Creating StartScreen Object");
+    buttonList.push(new Buttons(0, windowHeight / 2.3, windowWidth / 16, windowHeight / 15,0));
   }
 
   draw() {
     if(startscreen_draw) console.log("Drawing on Startscreen");
   }
+
+  // drawButton(){
+  //   /* push all my settings */
+  //   push();
+  //   /* translate my postion to the center of the screen */
+  //   translate(windowWidth / 2, windowHeight / 2);
+  //   /* Setting my strokeweight */
+  //   strokeWeight(4);
+  //   /* Setting my stroke color */
+  //   // stroke(0, 0, 0, mstartScreen.checkPlayButton());
+  //   /* Drawing my rectangle */
+  //   strokeWeight(0);
+  //   textSize(windowHeight / 30);
+  //   /* Setting style to Georgia because it looks good */
+  //   textFont('Georgia');
+  //   /* Write the back onto the box */
+  //   text("Play", 0, windowHeight / 2.25);
+  //   /* popping all my settings so other functions dont have to deal with them */
+  //   stroke(0, 0, 0, 255);
+  //   strokeWeight(1);
+  //   // fill(0, 0, 0, mstartScreen.checkPlayButton())
+  //   // rect(0, windowHeight / 2.3, windowWidth / 16, windowHeight / 15, 20);
+  //   buttonList.push(new Buttons(0, windowHeight / 2.3, windowWidth / 16, windowHeight / 15,0));
+  //   pop();
+  // }
 }
