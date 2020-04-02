0518e8515c3c2fa36c23e1c616e630fda44dd50a
javaComSci <iramanat@purdue.edu>
2020-04-02 00:26:20 -0400

    conenction

--- BackendExtreme/Backend/DatabaseManagement/SQLConnection.cs
+++ BackendExtreme/Backend/DatabaseManagement/SQLConnection.cs
@@ -164,9 +164,11 @@ public class SQLConnection
                     teamCount += 1;
 
                     // put the information read into the score info object
-                    int teamId = Convert.ToInt32(reader1[0]);
-                    string t = Convert.ToString(reader1[1]);
+                    string t = Convert.ToString(reader1[0]);
                     List<String> playerNames = new List<String>();
+                    if(reader1[1] != DBNull.Value) {
+                        playerNames.Add(Convert.ToString(reader1[1]));
+                    }
                     if(reader1[2] != DBNull.Value) {
                         playerNames.Add(Convert.ToString(reader1[2]));
                     }
@@ -176,12 +178,9 @@ public class SQLConnection
                     if(reader1[4] != DBNull.Value) {
                         playerNames.Add(Convert.ToString(reader1[4]));
                     }
-                    if(reader1[5] != DBNull.Value) {
-                        playerNames.Add(Convert.ToString(reader1[5]));
-                    }
-                    int teamScore = Convert.ToInt32(reader1[6]);
-                    int timePlayed = Convert.ToInt32(reader1[7]);
-                    int ranking = Convert.ToInt32(reader1[8]);
+                    int teamScore = Convert.ToInt32(reader1[5]);
+                    int timePlayed = Convert.ToInt32(reader1[6]);
+                    int ranking = Convert.ToInt32(reader1[7]);
 
                     // add the top team to the list of top teams
                     ScoresInfo team = new ScoresInfo(teamName, playerNames, teamScore, timePlayed);
@@ -243,9 +242,11 @@ public class SQLConnection
                 while(reader1.Read()) {
 
                     // put the information read into the score info object
-                    int teamId = Convert.ToInt32(reader1[0]);
-                    String teamName = Convert.ToString(reader1[1]);
+                    String teamName = Convert.ToString(reader1[0]);
                     List<String> playerNames = new List<String>();
+                    if(reader1[1] != DBNull.Value) {
+                        playerNames.Add(Convert.ToString(reader1[1]));
+                    }
                     if(reader1[2] != DBNull.Value) {
                         playerNames.Add(Convert.ToString(reader1[2]));
                     }
@@ -255,11 +256,8 @@ public class SQLConnection
                     if(reader1[4] != DBNull.Value) {
                         playerNames.Add(Convert.ToString(reader1[4]));
                     }
-                    if(reader1[5] != DBNull.Value) {
-                        playerNames.Add(Convert.ToString(reader1[5]));
-                    }
-                    int teamScore = Convert.ToInt32(reader1[6]);
-                    int timePlayed = Convert.ToInt32(reader1[7]);
+                    int teamScore = Convert.ToInt32(reader1[5]);
+                    int timePlayed = Convert.ToInt32(reader1[6]);
 
                     // add the top team to the list of top teams
                     ScoresInfo team = new ScoresInfo(teamName, playerNames, teamScore, timePlayed);
