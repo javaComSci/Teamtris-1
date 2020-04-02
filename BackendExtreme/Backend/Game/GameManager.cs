324e0cf8764330a7eb150e1e3aebb7387a1d1825
javaComSci <iramanat@purdue.edu>
2020-04-01 20:11:48 -0400

    thread

--- BackendExtreme/Backend/Game/GameManager.cs
+++ BackendExtreme/Backend/Game/GameManager.cs
@@ -86,6 +86,8 @@ public class GameManager
                         if (bob == null)
                         {
                             Console.WriteLine("no place to place piece");
+                            thread.Abort();
+                            return;
                         }
                         else
                         {
