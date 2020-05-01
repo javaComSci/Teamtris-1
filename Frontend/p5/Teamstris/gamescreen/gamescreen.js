5789b6394097c51c6ebbf02fdb1a5fe567465f96
Richard Hansengit config --global user.name Richard <richie.hansen@gmail.com>
2020-05-01 14:03:18 -0700

    changing to score keeper

--- Frontend/p5/Teamstris/gamescreen/gamescreen.js
+++ Frontend/p5/Teamstris/gamescreen/gamescreen.js
@@ -73,6 +73,8 @@ class GameScreen {
 
     // game score
     this.gameScore = 0;
+
+    this.finalScore = 0;
   }
 
   SetPlayerCount() {
@@ -110,7 +112,8 @@ class GameScreen {
   DrawScore() {
     textSize(32);
     fill(0, 255, 255);
-    text("Score: " + (this.gameScore + this.GameArray.gameArrayScore), this.CustomWindowWidth*0.1, this.CustomWindowHeight*0.05);
+    //text("Score: " + (this.gameScore + this.GameArray.gameArrayScore), this.CustomWindowWidth*0.1, this.CustomWindowHeight*0.05);
+    text("Score: " + (this.finalScore, this.CustomWindowWidth*0.1, this.CustomWindowHeight*0.05);
   }
 
   /**
@@ -186,6 +189,7 @@ class GameScreen {
 
         this.totalGameTime = e.current_time
         this.GameArray.totalGameTime = this.totalGameTime
+        this.finalScore = gameScore + e.score
         this.UpdateGameSpeed()
       } else if (e.type == 666) {
         team.score = this.gameScore;
